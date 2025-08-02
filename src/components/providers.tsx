f1a75b2f37a66e0c4d89244c188b70b579034afd
fardin072 <omurfaruquetalukder9@gmail.com>
2025-08-02 12:54:52 +0600

    Navigation bar problem solve

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,11 +3,13 @@
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from '@/components/ui/toaster'
 import { DataProvider } from '@/contexts/DataContext'
+import Navigation from './Navigation'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
       <DataProvider>
+        <Navigation/>
         {children}
         <Toaster />
       </DataProvider>
